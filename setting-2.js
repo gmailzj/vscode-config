27d82806d23c710660c294235f2962967552f8ed
lake <gmailzj@qq.com>
2018-07-02 10:08:35 +0800

    Create setting-2.js

---
+++ setting-2.js
@@ -0,0 +1,47 @@
+{
+    // Set the default
+    "editor.formatOnSave": false,
+    // Enable per-language
+    "[javascript]": {
+        "editor.formatOnSave": true
+    },
+    // 以像素为单位控制字号。
+    "editor.fontSize": 16,
+    // "editor.formatOnSave": true,
+    "editor.formatOnPaste": true,
+    "files.associations": {
+        ".eslintrc.json": "jsonc",
+        "*.vue": "vue"
+    },
+    "window.zoomLevel": 1,
+    "emmet.syntaxProfiles": {
+        "vue-html": "html",
+        "vue": "html"
+    },
+    "prettier.eslintIntegration": true,
+    "eslint.autoFixOnSave": true,
+    "eslint.validate": [
+        "javascript",
+        "javascriptreact",
+        // {
+        //     "language": "html",
+        //     "autoFix": true
+        // },
+        {
+            "language": "vue",
+            "autoFix": true
+        }
+    ],
+    "javascript.format.insertSpaceBeforeFunctionParenthesis": true,
+    // 强制单引号
+    // "prettier.singleQuote": true,
+    // 尾随逗号
+    "prettier.trailingComma": "all",
+    "emmet.includeLanguages": {
+        "vue-html": "html",
+        "javascript": "javascriptreact",
+        "postcss": "css"
+    },
+    "css.validate": false
+    
+}
